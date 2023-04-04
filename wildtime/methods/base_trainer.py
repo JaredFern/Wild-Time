f863bf8d7d8e70266c7119e8f62db6d7d9335bc3
Jared Fernandez <jaredfern@cmu.edu>
2023-04-04 16:12:10 -0400

    Add tqdm progress bar

--- wildtime/methods/base_trainer.py
+++ wildtime/methods/base_trainer.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn.functional as F
 from sklearn import metrics
 from tdc import Evaluator
+from tqdm import tqdm
 
 from .dataloaders import FastDataLoader, InfiniteDataLoader
 from .utils import prepare_data, forward_pass, get_collate_functions
@@ -72,7 +73,7 @@ class BaseTrainer:
     def train_step(self, dataloader):
         self.network.train()
         loss_all = []
-        for step, (x, y) in enumerate(dataloader):
+        for step, (x, y) in enumerate(tqdm(dataloader, total=self.args.train_update_iter)):
             x, y = prepare_data(x, y, str(self.train_dataset))
             loss, logits, y = forward_pass(x, y, self.train_dataset, self.network, self.criterion, self.lisa, self.mixup,
                                            self.cut_mix, self.mix_alpha)
